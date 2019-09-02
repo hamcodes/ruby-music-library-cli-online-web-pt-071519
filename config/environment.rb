bbe7c515046dff36f3442ff21e2f8eb7c409ee20
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-09-02 22:33:44 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,6 @@ Bundler.require
 module Concerns
 end
 
+require_all 'lib/concerns'
 require_all 'lib'
+
--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,11 @@ class Song
     end
   end
   
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+    
+  end
+  
   def initialize(name,artist=nil,genre=nil)
     @name = name 
     self.artist = artist
