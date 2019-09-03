ba5dcc342c9977d96d5ec8f36548ab0f57a2fe99
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-09-03 21:30:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -43,6 +43,21 @@ def self.create(name)
     artist.add_song(self) if artist
   end
   
+  def self.new_from_filename(file)
+    raw = file.gsub(".mp3", "")
+    row = raw.split(" - ")
+    song_name = row[1]
+    artist = Artist.find_or_create_by_name(row[0]) 
+    genre = Genre.find_or_create_by_name(row[2])
+    Song.new(song_name, artist, genre)
+  end
+  
+  def self.create_from_filename(file)
+    song = new_from_filename(file)
+    song.save
+    
+  end
+  
     def genre=(genre)
     @genre = genre
     genre.add_song(self) if genre
